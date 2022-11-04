f642f7369df1de55211554bf2a3ceef7702a7250
wuxuehuai <154670888@qq.com>
2022-11-04 11:28:27 +0800

    add transfer

--- src/home/Index.vue
+++ src/home/Index.vue
@@ -1,13 +1,24 @@
 <script setup>
 import Headers from './Header.vue'
-import { ref } from 'vue'
+import { ref, watch } from 'vue'
 import spf from './../index'
 const activeName = ref('web3')
 const spfWeb3WalletJson = JSON.parse(localStorage.getItem('spf-web3-wallet')) || []
 const dynamicTags = ref([...spfWeb3WalletJson])
 const spfPolkadotWalletJson = JSON.parse(localStorage.getItem('spf-polkadot-wallet')) || []
 const polkadotTags = ref([...spfPolkadotWalletJson])
-
+const loading = ref({
+    getBalance: false,
+    web3SendTransaction: false
+})
+watch(() => dynamicTags.value, (value) => {
+    updateDynamicTags(value)
+}, { deep: true })
+// 0x5F245F071103BB8bcC4Dd5ee4A57F15347514f58
+// 0x9b44a46e2645cb7db6e13aa8e7cf9b05f26d824375f011cfb7c98545e0ab9987
+// 0xe42E69E8B2480E9fFF0Dc1Cd3afd1D1fc253Cdd4
+// 0x5e67cfd304b92ecb404f204f8799dc1d5032939a939dbf638d08a1098162a198
+// spf.web3SendTransaction({ from: '0x9b44a46e2645cb7db6e13aa8e7cf9b05f26d824375f011cfb7c98545e0ab9987', to: '0xe42E69E8B2480E9fFF0Dc1Cd3afd1D1fc253Cdd4', value: '0.001'})
 const updateDynamicTags = (value) => {
     localStorage.setItem('spf-web3-wallet', JSON.stringify(value))
 }
@@ -16,12 +27,10 @@ const updatePolkadotTags = (value) => {
 }
 const handleClose = (index) => {
     dynamicTags.value.splice(index, 1)
-    updateDynamicTags(dynamicTags.value)
 }
 const newWallet = () => {
     const wallet = spf.web3NewWallet()
     dynamicTags.value.push(wallet)
-    updateDynamicTags(dynamicTags.value)
 }
 
 const handlePolkadotClose = (index) => {
@@ -33,6 +42,19 @@ const newPolkadotWallet = async () => {
     const wallet = await spf.polkadotNewWallet()
     polkadotTags.value.push(wallet)
     updatePolkadotTags(polkadotTags.value)
+}
+const getBalance = async (address, index) => {
+    loading.value.getBalance = true
+    const balance = await spf.web3Wallet(address)
+    dynamicTags.value[index].balance = balance
+    loading.value.getBalance = false
+}
+const web3SendTransaction = async (from, to, value, address, index) => {
+    loading.value.web3SendTransaction = true
+    const res = await spf.web3SendTransaction({ from, to, value})
+    loading.value.web3SendTransaction = false
+    getBalance(address, index)
+    dynamicTags.value[index].value = ''
 }
 
 </script>
@@ -65,6 +87,24 @@ const newPolkadotWallet = async () => {
                                     <el-descriptions-item label="keyStore" label-align="right" align="center"> {{
                                             tag.keyStore
                                     }}
+                                    </el-descriptions-item>
+                                    <el-descriptions-item label="balance" label-align="right" align="center">
+                                        <div class="descriptions-tag-balance">
+                                            <template v-if="tag.balance">
+                                                <div style="margin-right: 10px;"> {{ tag.balance }}</div>
+                                            </template>
+                                            <el-button :loading="loading.getBalance"
+                                                @click="getBalance(tag.account.address, index)" type="primary">{{
+                                                        tag.balance ? `Update` : `Gain`
+                                                }}</el-button>
+                                        </div>
+                                    </el-descriptions-item>
+                                    <el-descriptions-item label="operation" label-align="right" align="center">
+                                        <div class="descriptions-tag-balance">
+                                            <el-input style="margin-right: 10px;" v-model="tag.to" placeholder="address" />
+                                            <el-input style="margin-right: 10px; width: 200px;" v-model="tag.value" placeholder="value" />
+                                            <el-button :loading="loading.web3SendTransaction" type="primary" @click="web3SendTransaction(tag.account.privateKey, tag.to, tag.value, tag.account.address, index)">Transaction</el-button>
+                                        </div>
                                     </el-descriptions-item>
                                 </el-descriptions>
                             </template>
@@ -135,5 +175,12 @@ const newPolkadotWallet = async () => {
 
 .descriptions-label-class {
     width: 100px;
+}
+
+.descriptions-tag-balance {
+    width: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
 }
 </style>
--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import Web3 from "web3"
 import { ApiPromise, Keyring } from '@polkadot/api'
 import { cryptoWaitReady, mnemonicGenerate } from '@polkadot/util-crypto'
+const web3 = new Web3(new Web3.providers.HttpProvider('https://rinkeby.infura.io/v3/3398e48e733d4b9bb337702c05a77c2d'))
+
 const web3NewWallet = () => {
-    const web3 = new Web3();
     const account = web3.eth.accounts.create()
     return {
         account: {
@@ -17,6 +18,7 @@ const polkadotNewWallet = async () => {
     const mnemonic = mnemonicGenerate()
     const keyring = new Keyring({ type: 'sr25519' })
     const pair = keyring.addFromUri(mnemonic);
+    console.log(keyring.encodeAddress(pair.publicKey))
     const ss58Address = {
         address: pair.address,
         publicKey: pair.publicKey.join(',')
@@ -26,4 +28,21 @@ const polkadotNewWallet = async () => {
         mnemonic
     }
 }
-export default { Web3, Polkadot: ApiPromise, web3NewWallet, polkadotNewWallet}
+const web3Wallet = async (address) => {
+    const account = await web3.eth.getBalance(address)
+    return web3.utils.fromWei(account, 'ether')
+}
+const web3SendTransaction = async ({ from, to, value }) => {
+    const account = web3.eth.accounts.wallet.add(from)
+    const gasPrice = 30 * 1e9
+    const signed = await account.signTransaction({
+        from: account.address,
+        to,
+        value: web3.utils.toWei(value, 'ether'),
+        gasLimit: web3.utils.toHex(21000),
+        gasPrice: gasPrice
+    })
+    const hash = await web3.eth.sendSignedTransaction(signed.rawTransaction);
+    return hash
+}
+export default { Web3, Polkadot: ApiPromise, web3NewWallet, polkadotNewWallet, web3Wallet, web3SendTransaction }
